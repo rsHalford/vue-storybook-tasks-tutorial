80d5a2c4126f61f653a79f7ef66109d4bf4bf9b1
Richard Halford <richardh.1995@googlemail.com>
2020-10-30 15:01:32 +0000

    Storybook Tutorial setup

---
+++ tests/unit/TaskList.spec.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import PureTaskList from '../../src/components/PureTaskList.vue';
+import { withPinnedTasksData } from '../../src/components/PureTaskList.stories';
+
+it('renders pinned tasks at the start of the list', () => {
+  const Constructor = Vue.extend(PureTaskList);
+  const vm = new Constructor({
+    propsData: { tasks: withPinnedTasksData },
+  }).$mount();
+  const firstTaskPinned = vm.$el.querySelector('.list-item:nth-child(1).TASK_PINNED');
+
+  // We expect the pinned task to be rendered first, not at the end
+  expect(firstTaskPinned).not.toBe(null);
+});
